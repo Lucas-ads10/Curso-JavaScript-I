89212893baa3414079d171b8773fa6c55f98f168
Lucas Silva <lucasdssilva13@hotmail.com>
2023-08-21 22:27:54 -0300

    Atualização e pagina html da calculadora

--- Exercicios/Calculadora_de_4_operações/index.js
+++ Exercicios/Calculadora_de_4_operações/index.js
@@ -1,48 +1,29 @@
-const register ={
-    nome: "",
-    sobrenome: "",
-    estudo: "",
-    ano:""
-}
-function pegarValores(evento){
-    register.nome = document.getElementById('name').value
-    register.sobrenome = document.getElementById('surname').value
-    register.estudo = document.getElementById('study').value
-    register.ano = document.getElementById('birthday').value
+var nome = "";
+var sobrenome = "";
+var estudo = "";
+var ano = "";
+var primeiroNumero = "";
+var segundoNumero = "";
 
-    if (register.nome != '' || register.sobrenome != '' ||  register.estudo != '' ||  register.ano != ''){
-        document.write("Nome: " + nome + "<br>" + "Sobrenome: " + sobrenome "Estudo: " + estudo + "<br>" "Ano: " + ano +)
+function capturar(){
+    nome = document.getElementById('name').value;
+    sobrenome = document.getElementById('surname').value;
+    estudo = document.getElementById('study').value;
+    ano = document.getElementById('birthday').value;
+
+    if (nome != '' , sobrenome != '', estudo != '', ano != '' ){
+        window.location.href = "calculadora.html";
     }else{
-        alert('Campos incompletos')
+        alert('Campos incompletos');
     }
+    
+    sessionStorage.setItem('valorDigitado', nome);
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-// Obtém o botão pelo ID
-    const botao = document.getElementById("button");
-    const inputElement = document.getElementById("name");
-// Adiciona um evento de clique ao botão
-
-    botao.addEventListener("submit", function (){
-  
-    function.preventDefault();
-        
-    // Obtém o valor do input quando o botão é clicado
-    const nameOne = inputElement.value;
-  
-  // Agora, você pode usar valorDoInput da forma que desejar
-    console.log("O valor do input é: " + nameOne);
+function calculo(){
+    primeiroNumero = document.getElementById('firstNumber').value;
+    segundoNumero = document.getElementById('secondNumber').value;
 }
-    alert("O valor do input é: " + nameOne);
+
